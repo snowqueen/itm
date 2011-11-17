5cc6a52f3dea5e58e16371fed0b6cc2750297573
Snowqueen <szobacsere@gmail.com>
2011-11-17 20:42:57 +0100

    Linkek megjelenesenek finomitasa Uj komment es komment szerkesztes a slug url-re visznek

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -29,12 +29,13 @@ class CommentsController < ApplicationController
     begin
       @comment = Comment.find(params[:id])
       if @comment.user == @current_user
-        redirect_to post_path(@comment.post, :edit_comment => @comment, :anchor => "comment-#{@comment.id}")
+        redirect_to post_slug_path(@comment.post.slug, :edit_comment => @comment, :anchor => "comment-#{@comment.id}")
       else
         logger.info "#{@comment.user.name} != #{@current_user}"
-        redirect_to @comment.post
+        redirect_to post_slug_path(@comment.post)
       end
     rescue Exception => e
+      logger.error e
       logger.error "No comment with id: #{params[:id]}"
       redirect_to root_url
     end
@@ -48,11 +49,9 @@ class CommentsController < ApplicationController
     @post = Post.find(params[:post_id])
     @comment.post = @post
 
-    logger.info "Comment #{@comment}, Post #{@post}"
-
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to post_path(@post, :anchor => "comment-#{@comment.id}"), notice: 'Comment was successfully created.' }
+        format.html { redirect_to post_slug_path(@post.slug, :anchor => "comment-#{@comment.id}"), notice: 'Comment was successfully created.' }
         format.json { render json: @comment, status: :created, location: @comment }
       else
         format.html { render :template => "posts/show" }
@@ -68,7 +67,7 @@ class CommentsController < ApplicationController
 
     respond_to do |format|
       if @comment.update_attributes(params[:comment])
-        format.html { redirect_to post_path(@comment.post, :anchor => "comment-#{@comment.id}"), notice: 'Comment was successfully updated.' }
+        format.html { redirect_to post_slug_path(@comment.post.slug, :anchor => "comment-#{@comment.id}"), notice: 'Comment was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
